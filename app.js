f9f5d220022583e424297eafb7eff54ad4be3009
aliezaheer <alizaheer67@gmail.com>
2022-11-01 18:40:14 +0500

    getting data from the server

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-
+  //   console.log(req.url, req.method, req.headers);
   //   process.exit(); //This line of code stops the server by stoping loop of listner
 
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter the text.</title></head>");
+    res.write(
+      "<body><form action=/message method=POST><input type=text><button type=submit>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My Node Js Page</title></head>");
   res.write("<body><h1>My Node Js Server, Build by Ali Zaheer.</h1></body>");
   res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
