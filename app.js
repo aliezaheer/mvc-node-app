e2f00684b82a0c0bdcf23fff0db789c1068ebd04
aliezaheer <alizaheer67@gmail.com>
2022-11-07 15:16:05 +0500

    middleware setup in express

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("A middleware res.");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Second middleware res.");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
