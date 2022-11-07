bb568fbafa0f48b2fbd76e7b034853a5128ee587
aliezaheer <alizaheer67@gmail.com>
2022-11-07 22:05:40 +0500

    testing page

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const path = require("path");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
