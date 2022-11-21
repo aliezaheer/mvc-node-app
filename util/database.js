d2c1e06194d183106c448491069b8bf64b7ff6e1
aliezaheer <alizaheer67@gmail.com>
2022-11-21 23:01:46 +0500

    creating modal in sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-shop", "root", "alizahee#67", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-shop",
-  password: "AliZahee#67",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
