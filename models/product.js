bb3314c43202f94cd1576608ff9a3182a85b51b7
aliezaheer <alizaheer67@gmail.com>
2022-11-24 15:40:55 +0500

    changing mehtods as per sequelize

--- models/product.js
+++ models/product.js
@@ -6,21 +6,19 @@ const Product = sequelize.define("product", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
-    allowNull: false,
     primaryKey: true,
+    defaultValue: null,
   },
   title: Sequelize.STRING,
-  allowNull: false,
+
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
   },
   ImageUrl: {
     type: Sequelize.STRING,
   },
   description: {
     type: Sequelize.STRING,
-    allowNull: false,
   },
 });
 
